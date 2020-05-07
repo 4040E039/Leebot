edffe4684f813c218dde000c3f15e19302595a00
4040E039 <david19961223@gmail.com>
2020-05-07 21:02:30 +0800

    Add files via upload

--- index.js
+++ index.js
@@ -12,15 +12,52 @@ const bot = linebot({
   channelSecret: process.env.CHANNEL_SECRET,
   channelAccessToken: process.env.CHANNEL_ACCESS_TOKEN
 })
-bot.on('message', async (event) => {
-  let msg = event.message.text;
+
+
+bot.on('join', async (event) => {
   try {
-    if (msg === '你好' || msg === 'Hello' || msg === 'Hi'){
+    event.reply('大家好')
+  } catch (error) {
+    msg = '發生錯誤'
+  }
+})
+bot.on('leave', async (event) => {
+  try {
+    event.reply('我滾啦')
+  } catch (error) {
+    msg = '發生錯誤'
+  }
+})
+let messageData = () => {
+
+  let msg = event.message.text;
+
+  switch (msg){
+    case '你好' || 'Hello' || 'Hi' :
       event.reply(msg)
-    }
-    else if (msg === '愛你' || msg === '我愛你'){
-      event.reply({AccountName} +'我也愛你')
-    }
+      break;
+    case '愛你' || '我愛你':
+      event.reply(msg)
+      break;
+    default:
+      event.reply('沒有相對應文字')
+
+  }
+
+ 
+  /* 
+  if (msg === '你好' || msg === 'Hello' || msg === 'Hi'){
+    event.reply(msg)
+  }
+  else if (msg === '愛你' || msg === '我愛你'){
+    event.reply('我也愛你')
+  }else{
+    event.reply('沒有相對應文字')
+  } */
+}
+bot.on('message', async (event) => {
+  try {
+    messageData();
     // const data = await rp({ uri: 'https://kktix.com/events.json', json: true })
     // msg = data.entry[0].title
   } catch (error) {
