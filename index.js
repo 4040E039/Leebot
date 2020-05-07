5c1ce18ccd07aac8516e8d4c39b11fd1a10a0a00
4040E039 <david19961223@gmail.com>
2020-05-08 01:22:19 +0800

    Add files via upload

--- index.js
+++ index.js
@@ -45,74 +45,74 @@ bot.on('message', async (event) => {
       msgdata = data[0].Bankcashbuy
       event.reply('美金: '+msgdata)
     }
-    // else if ('!港幣'){
-    //   msgdata = data[1].Bankcashbuy
-    //   event.reply('港幣: '+msgdata)
-    // }
-    // else if ('!英鎊'){
-    //   msgdata = data[2].Bankcashbuy
-    //   event.reply('英鎊: '+msgdata)
-    // }
-    // else if ('!加拿大幣'){
-    //   msgdata = data[4].Bankcashbuy
-    //   event.reply('加拿大幣: '+msgdata)
-    // }
-    // else if ('!澳幣'){
-    //   msgdata = data[5].Bankcashbuy
-    //   event.reply('澳幣: '+msgdata)
-    // }
-    // else if ('!法郎'){
-    //   msgdata = data[6].Bankcashbuy
-    //   event.reply('法郎: '+msgdata)
-    // }
-    // else if ('!日圓'){
-    //   msgdata = data[7].Bankcashbuy
-    //   event.reply('日圓: '+msgdata)
-    // }
-    // else if ('!南非幣'){
-    //   msgdata = data[8].Bankcashbuy
-    //   event.reply('南非幣: '+msgdata)
-    // }
-    // else if ('!瑞典幣'){
-    //   msgdata = data[9].Bankcashbuy
-    //   event.reply('瑞典幣: '+msgdata)
-    // }
-    // else if ('!紐元'){
-    //   msgdata = data[10].Bankcashbuy
-    //   event.reply('紐元: '+msgdata)
-    // }
-    // else if ('!泰幣'){
-    //   msgdata = data[11].Bankcashbuy
-    //   event.reply('泰幣: '+msgdata)
-    // }
-    // else if ('!菲國比索'){
-    //   msgdata = data[12].Bankcashbuy
-    //   event.reply('菲國比索: '+msgdata)
-    // }
-    // else if ('!印尼幣'){
-    //   msgdata = data[13].Bankcashbuy
-    //   event.reply('印尼幣: '+msgdata)
-    // }
-    // else if ('!歐元'){
-    //   msgdata = data[14].Bankcashbuy
-    //   event.reply('歐元: '+msgdata)
-    // }
-    // else if ('!韓元'){
-    //   msgdata = data[15].Bankcashbuy
-    //   event.reply('韓元: '+msgdata)
-    // }
-    // else if ('!越南盾'){
-    //   msgdata = data[16].Bankcashbuy
-    //   event.reply('越南盾: '+msgdata)
-    // }
-    // else if ('!馬來幣'){
-    //   msgdata = data[17].Bankcashbuy
-    //   event.reply('馬來幣: '+msgdata)
-    // }
-    // else if ('!人民幣'){
-    //   msgdata = data[18].Bankcashbuy
-    //   event.reply('人民幣: '+msgdata)
-    // }
+    else if (msg === '!港幣'){
+      msgdata = data[1].Bankcashbuy
+      event.reply('港幣: '+msgdata)
+    }
+    else if (msg === '!英鎊'){
+      msgdata = data[2].Bankcashbuy
+      event.reply('英鎊: '+msgdata)
+    }
+    else if (msg === '!加拿大幣'){
+      msgdata = data[4].Bankcashbuy
+      event.reply('加拿大幣: '+msgdata)
+    }
+    else if (msg === '!澳幣'){
+      msgdata = data[5].Bankcashbuy
+      event.reply('澳幣: '+msgdata)
+    }
+    else if (msg === '!法郎'){
+      msgdata = data[6].Bankcashbuy
+      event.reply('法郎: '+msgdata)
+    }
+    else if (msg === '!日圓'){
+      msgdata = data[7].Bankcashbuy
+      event.reply('日圓: '+msgdata)
+    }
+    else if (msg === '!南非幣'){
+      msgdata = data[8].Bankcashbuy
+      event.reply('南非幣: '+msgdata)
+    }
+    else if (msg === '!瑞典幣'){
+      msgdata = data[9].Bankcashbuy
+      event.reply('瑞典幣: '+msgdata)
+    }
+    else if (msg === '!紐元'){
+      msgdata = data[10].Bankcashbuy
+      event.reply('紐元: '+msgdata)
+    }
+    else if (msg === '!泰幣'){
+      msgdata = data[11].Bankcashbuy
+      event.reply('泰幣: '+msgdata)
+    }
+    else if (msg === '!菲國比索'){
+      msgdata = data[12].Bankcashbuy
+      event.reply('菲國比索: '+msgdata)
+    }
+    else if (msg === '!印尼幣'){
+      msgdata = data[13].Bankcashbuy
+      event.reply('印尼幣: '+msgdata)
+    }
+    else if (msg === '!歐元'){
+      msgdata = data[14].Bankcashbuy
+      event.reply('歐元: '+msgdata)
+    }
+    else if (msg === '!韓元'){
+      msgdata = data[15].Bankcashbuy
+      event.reply('韓元: '+msgdata)
+    }
+    else if (msg === '!越南盾'){
+      msgdata = data[16].Bankcashbuy
+      event.reply('越南盾: '+msgdata)
+    }
+    else if (msg === '!馬來幣'){
+      msgdata = data[17].Bankcashbuy
+      event.reply('馬來幣: '+msgdata)
+    }
+    else if (msg === '!人民幣'){
+      msgdata = data[18].Bankcashbuy
+      event.reply('人民幣: '+msgdata)
+    }
     else if (msg === '你好' || msg === 'Hello' || msg === 'Hi'){
       event.reply(msg)
     }
