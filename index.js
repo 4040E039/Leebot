79e2d7089776837072a31be03fa1bbd7487ff622
4040E039 <david19961223@gmail.com>
2020-05-08 22:41:21 +0800

    Add files via upload

--- index.js
+++ index.js
@@ -124,7 +124,8 @@ bot.on('message', async (event) => {
         latitude: 22.967422,
         longitude: 120.297990
       }
-      nmsg = locationData + '\n' + '062396777' 
+      nmsg = locationData 
+      event.reply('062396777')
     }
     else if (msg === '!滾' || msg === '!閉嘴' || msg === '!B嘴') {
       nmsg = {
