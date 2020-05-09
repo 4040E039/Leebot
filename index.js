3453f61deacfa0c300cb006c8913d3b14e99241d
4040E039 <david19961223@gmail.com>
2020-05-09 16:21:25 +0800

    Add files via upload

--- index.js
+++ index.js
@@ -196,8 +196,20 @@ bot.on('message', async (event) => {
     } else if (msg === '!元之氣') {
       nmsg = '元之氣(歸仁中山店)' + '\n' + ' 062308846'
     } else if (msg === '!紅太陽') {
-      nmsg = '紅太陽(歸仁中山店)' + '\n' + ' 062303456'
-    } else if (msg === '!小南便當') {
+      nmsg = [{
+        type: 'image',
+        originalContentUrl: 'https://4040e039.github.io/image/redsun1.jpg',
+        previewImageUrl: 'https://4040e039.github.io/image/redsun1.jpg'
+      },{
+        type: 'image',
+        originalContentUrl: 'https://4040e039.github.io/image/redsun2.jpg',
+        previewImageUrl: 'https://4040e039.github.io/image/redsun2.jpg'
+      }, {
+        type: 'text',
+        text: '紅太陽(歸仁中山店)' + '\n' + ' 062303456'
+      }]
+    } 
+    else if (msg === '!小南便當') {
       nmsg = '小南雞腿排骨大王(歸仁店)' + '\n' + ' 063302418'
     } else if (msg === '!滾' || msg === '!閉嘴' || msg === '!B嘴') {
       nmsg = {
