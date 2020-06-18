4f8df8679ebb1158beb50033bc098156f4a7072f
Lee <david19961223@gmail.com>
2020-06-18 20:08:20 +0800

    push

--- Updeta.bat
+++ Updeta.bat
@@ -1,1 +1,2 @@
+cd C:\Users\GOD\Desktop\Leebot
 node C:\Users\GOD\Desktop\WorkZone\Leebot\exchangerate.js
--- push.bat
+++ push.bat
@@ -1,3 +1,4 @@
+cd C:\Users\GOD\Desktop\Leebot
 git add .
 git commit -m push
 git push psuh3 master
